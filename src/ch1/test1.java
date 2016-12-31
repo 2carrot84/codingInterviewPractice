842f1606168e31cbeeb99311e36d78a637a073f6
eguns <eguns@egunsui-MacBook-Air.local>
2016-12-31 12:55:20 +0900

    Initial commit

---
+++ src/ch1/test1.java
@@ -0,0 +1,35 @@
+package ch1;
+
+/**
+ * Created by eguns on 2016. 12. 20..
+ */
+public class test1 {
+    public static void main(String[] args) {
+        isUnique("가나다라12가34");
+
+    }
+
+    public static void isUnique(String str) {
+        boolean flag = true;
+
+        for (int i = 0; i < (str.length()-1); i++) {
+            for (int j = (i+1); j < str.length(); j++) {
+                if(str.charAt(i) == str.charAt(j)) {
+                    System.out.println("str.charAt(" + i + ") : " + str.charAt(i));
+                    System.out.println("str.charAt(" + j + ") : " + str.charAt(j));
+
+                    flag = false;
+                    break;
+                }
+            }
+            if(!flag) break;
+        }
+
+        if(flag) {
+            System.out.println("모든 문자가 유일합니다.");
+        }
+        else {
+            System.out.println("모든 문자가 유일하지 않습니다.");
+        }
+    }
+}
